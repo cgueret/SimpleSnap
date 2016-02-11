6a9803c4e99298eacc30633d35eb204070da8bb7
Christophe Gueret <christophe.gueret@bbc.co.uk>
2016-02-11 23:20:02 +0000

    Working base

--- test.py
+++ test.py
@@ -18,35 +18,37 @@ class GTK_Main(object):
         vbox = Gtk.VBox()
         window.add(vbox)
         hbox = Gtk.HBox()
-        vbox.pack_start(hbox, False, False, 0)
-        self.entry = Gtk.Entry()
-        hbox.add(self.entry)
-        self.button = Gtk.Button("Start")
-        hbox.pack_start(self.button, False, False, 0)
-        self.button.connect("clicked", self.start_stop)
         self.movie_window = Gtk.DrawingArea()
         vbox.add(self.movie_window)
+        vbox.pack_start(hbox, False, False, 0)
+        self.button = Gtk.Button("Click")
+        hbox.pack_start(self.button, True, True, 0)
         window.show_all()
         
         
-        self.player = Gst.ElementFactory.make("playbin", "player")
+        self.player = Gst.Pipeline.new("player")
+        source = Gst.ElementFactory.make("v4l2src", "webcam")
+        mirror = Gst.ElementFactory.make("videoflip", "webcam-flip")
+        mirror.set_property("method", "horizontal-flip")
+        colorspace = Gst.ElementFactory.make("videoconvert")
+        sink = Gst.ElementFactory.make("autovideosink", "app")
+        sink.set_property("sync", False)
+        
+        self.player.add(source)
+        self.player.add(mirror)
+        self.player.add(colorspace)
+        self.player.add(sink)
+        source.link(mirror)
+        mirror.link(colorspace)
+        colorspace.link(sink)
+        
         bus = self.player.get_bus()
         bus.add_signal_watch()
         bus.enable_sync_message_emission()
         bus.connect("message", self.on_message)
         bus.connect("sync-message::element", self.on_sync_message)
-        
-    def start_stop(self, w):
-        if self.button.get_label() == "Start":
-            filepath = self.entry.get_text().strip()
-            if os.path.isfile(filepath):
-                filepath = os.path.realpath(filepath)
-                self.button.set_label("Stop")
-                self.player.set_property("uri", "file://" + filepath)
-                self.player.set_state(Gst.State.PLAYING)
-            else:
-                self.player.set_state(Gst.State.NULL)
-                self.button.set_label("Start")
+
+        self.player.set_state(Gst.State.PLAYING)        
 
     def on_message(self, bus, message):
         t = message.type
@@ -56,10 +58,11 @@ class GTK_Main(object):
         elif t == Gst.MessageType.ERROR:
             self.player.set_state(Gst.State.NULL)
             err, debug = message.parse_error()
-            print "Error: %s" % err, debug
+            print ("Error: %s" % err, debug)
             self.button.set_label("Start")
         
     def on_sync_message(self, bus, message):
+        print ("hello")
         if message.get_structure().get_name() == 'prepare-window-handle':
             imagesink = message.src
             imagesink.set_property("force-aspect-ratio", True)
