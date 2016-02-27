3d4fa4ed5b630561db83d32894b624beeb016304
Christophe Gueret <christophe.gueret@bbc.co.uk>
2016-02-27 15:19:59 +0000

    More testing

--- test.py
+++ test.py
@@ -10,69 +10,79 @@ from gi.repository import GdkPixbuf
 class GTK_Main(object):
     def __init__(self):
         window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
+        #window.set_resizable(False)
+        window.set_position(Gtk.WindowPosition.CENTER)
         window.set_title("Video-Player")
         window.set_default_size(500, 400)
         window.connect("destroy", Gtk.main_quit, "WM destroy")
         vbox = Gtk.VBox()
         window.add(vbox)
         hbox = Gtk.HBox()
-        self.movie_window = Gtk.DrawingArea()
-        vbox.add(self.movie_window)
+        gtksink = Gst.ElementFactory.make("gtksink", "app") # TODO check gtkglsink
+        gtksink.set_property("sync", False)
+        vbox.add(gtksink.get_property('widget'))
         vbox.pack_start(hbox, False, False, 0)
         button = Gtk.Button("Click")
         button.connect("clicked", self.on_click)
         hbox.pack_start(button, True, True, 0)
         window.show_all()
         
-        
+        # Prepare the pipeline        
         self.player = Gst.Pipeline.new("player")
+        
+        # Add the webcam as a source
         source = Gst.ElementFactory.make("v4l2src", "webcam")
+        self.player.add(source)
+        
+        # Mirror the image
         mirror = Gst.ElementFactory.make("videoflip", "webcam-flip")
         mirror.set_property("method", "horizontal-flip")
-        colorspace = Gst.ElementFactory.make("videoconvert")
-        sink = Gst.ElementFactory.make("autovideosink", "app") # TODO check gtkglsink
-        sink.set_property("sync", False)
-        
-        self.player.add(source)
         self.player.add(mirror)
-        self.player.add(colorspace)
-        self.player.add(sink)
         source.link(mirror)
+        
+        # Add a colorspace
+        colorspace = Gst.ElementFactory.make("videoconvert")
+        self.player.add(colorspace)
         mirror.link(colorspace)
-        colorspace.link(sink)
+        
+        # Split the channel
+        tee = Gst.ElementFactory.make("tee")
+        self.player.add(tee)
+
+        # Add the gtksink on one branch    
+        gtkqueue = Gst.ElementFactory.make("queue", "gtkqueue")
+        gtkqueue.link(gtksink)
+        self.player.add(gtkqueue)
+        gtkpad = tee.get_request_pad('src_%u')
+        print ("Obtained request pad {} for gtk".format( gtkpad.get_name()))
+        a = gtkpad.link(gtkqueue.get_static_pad('sink'))
+        print (a)
+        
+        # and the gdkpixbuf on the other
+        pixbufqueue = Gst.ElementFactory.make("queue")
+        self.player.add(pixbufqueue)
+        pixbuf = Gst.ElementFactory.make("gdkpixbufsink")
+        self.player.add(pixbuf)
+        pixbufqueue.link(pixbuf)
+        pixbufpad = tee.get_request_pad('src_%u')
+        print ("Obtained request pad {} for pixbuf".format( pixbufpad.get_name()))
+        a = pixbufpad.link(pixbufqueue.get_static_pad('sink'))
+        print (a)
+        
         
         bus = self.player.get_bus()
         bus.add_signal_watch()
         bus.enable_sync_message_emission()
-        bus.connect("message", self.on_message)
-        bus.connect("sync-message::element", self.on_sync_message)
 
         self.player.set_state(Gst.State.PLAYING)        
 
     def on_click(self, button):
         print ("Hello")
-        img = GdkPixbuf.Pixbuf.new(GdkPixbuf.Colorspace.RGB, True, 8, 800, 600 )
-        img.get_from_window(self.movie_window.window, self.movie_window.window.get_colormap(), 0, 0, 0, 0, 800, 600)
+        #self.sink.save('a.jpg','jpeg', {'quality': '100'})
         buffer = self.player.emit('convert-sample', Gst.Caps.from_string('image/png'))
         with open('frame.png', 'w') as fh:
             fh.write(str(buffer))
         
-    def on_message(self, bus, message):
-        t = message.type
-        if t == Gst.MessageType.EOS:
-            self.player.set_state(Gst.State.NULL)
-            self.button.set_label("Start")
-        elif t == Gst.MessageType.ERROR:
-            self.player.set_state(Gst.State.NULL)
-            err, debug = message.parse_error()
-            print ("Error: %s" % err, debug)
-            self.button.set_label("Start")
-        
-    def on_sync_message(self, bus, message):
-        if message.get_structure().get_name() == 'prepare-window-handle':
-            imagesink = message.src
-            imagesink.set_property("force-aspect-ratio", True)
-            imagesink.set_window_handle(self.movie_window.get_property('window').get_xid())
 
 GObject.threads_init()
 Gst.init(None)
