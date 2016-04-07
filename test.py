dd1e596ce81e96f44989fd9fc788a1a75e7d405b
Christophe Gueret <christophe.gueret@gmail.com>
2016-04-07 22:00:54 +0100

    Trying to use uvch264src

--- test.py
+++ test.py
@@ -9,30 +9,6 @@ from gi.repository import GdkPixbuf
 
 class GTK_Main(object):
     def __init__(self):
-        # Prepare the Gstreamer elements
-
-        # -> The sink to show the current image
-        gtksink = Gst.ElementFactory.make("gtksink", "view") # TODO check gtkglsink
-        gtksink.set_property("sync", False)
-        
-        # -> Camerabin
-        # TODO look at using http://www.oz9aec.net/index.php/gstreamer/487-using-the-logitech-c920-webcam-with-gstreamer-12
-        source = Gst.ElementFactory.make("camerabin", "webcam")
-        source.set_property('viewfinder-sink', gtksink)
-        source.set_property('mute', True)
-        caps =  Gst.Caps.from_string('image/png,width=1920,height=1080')
-        print(caps.to_string())
-        #source.set_property('image-capture-caps', caps)
-        
-        # -> prepare the pipeline        
-        self.player = Gst.Pipeline.new("player")
-        self.player.add(source)
-        
-        # -> Listen to the bus
-        bus = self.player.get_bus()
-        bus.add_signal_watch()
-        bus.enable_sync_message_emission()
-
         # Prepare the main window
         window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
         window.set_position(Gtk.WindowPosition.CENTER)
@@ -42,24 +18,55 @@ class GTK_Main(object):
         vbox = Gtk.VBox()
         window.add(vbox)
         hbox = Gtk.HBox()
-        vbox.add(gtksink.get_property('widget'))
+        self.movie_window = Gtk.DrawingArea()
+        vbox.add(self.movie_window)
         vbox.pack_start(hbox, False, False, 0)
         button = Gtk.Button("Click")
         button.connect("clicked", self.on_click)
         hbox.pack_start(button, True, True, 0)
         window.show_all()
                 
+        # Prepare the Gstreamer elements
+        pipeline = "uvch264src device=/dev/video1 name=src auto-start=true src.vfsrc"
+        pipeline += "! video/x-raw,format=(string)YUY2,width=320,height=240,framerate=30/1 !"
+        pipeline += "xvimagesink sync=false"
+        self.player = Gst.parse_launch(pipeline)
+        
+        # -> Listen to the bus
+        bus = self.player.get_bus()
+        bus.add_signal_watch()
+        bus.enable_sync_message_emission()
+        bus.connect("message", self.on_message)
+        bus.connect("sync-message::element", self.on_sync_message)
+
         self.player.set_state(Gst.State.PLAYING)        
 
     def on_click(self, button):
         print ("Hello")
-        camera = self.player.get_by_name('webcam')
-        camera.set_property('location', 'capture.jpg')
+        camera = self.player.get_by_name('src')
+        camera.set_property('mode', Gst.CameraBin.Mode.MODE_IMAGE)
+        print (camera.get_property('ready-for-capture'))
         camera.emit('start-capture')
-        a = camera.get_property('image-capture-supported-caps')
-        aa = a.to_string().split(';')
-        for a in aa:
-            print (a)
+
+    def on_message(self, bus, message):
+        if message.type == Gst.MessageType.WARNING:
+            err, debug = message.parse_warning()
+            print ("Error: %s" % err, debug)
+
+    def on_sync_message(self, bus, message):
+        struct = message.get_structure()
+        if not struct:
+            return
+        message_name = struct.get_name()
+        print (message_name)
+        print (message.src)
+        if message_name == "prepare-window-handle":
+            print (dir(message.src))
+            print (self.movie_window.window)
+            # Assign the viewport
+            imagesink = message.src
+            imagesink.set_property("force-aspect-ratio", True)
+            imagesink.set_xwindow_id(self.movie_window.window.xid)
         
 
 GObject.threads_init()
