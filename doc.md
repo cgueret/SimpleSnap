477be885f085fd3988af2637c1c1348bb23f4e69
Christophe Gueret <christophe.gueret@gmail.com>
2016-04-10 20:56:30 +0100

    First fully working h264 version

--- doc.md
+++ doc.md
@@ -9,3 +9,15 @@ https://lazka.github.io/pgi-docs/Gst-1.0/structs/Caps.html
 https://gstreamer.freedesktop.org/data/doc/gstreamer/head/gst-plugins-bad-libs/html/index.html
 
 http://kakaroto.homelinux.net/2012/09/uvc-h264-encoding-cameras-support-in-gstreamer/
+
+gst-launch-1.0 -v -e uvch264src device=/dev/video1 name=src auto-start=true \
+src.vfsrc ! queue ! video/x-raw,format=\(string\)BGRA,width=320,height=240,framerate=30/1 ! gtksink sync=false \
+src.vidsrc ! queue ! video/x-h264,width=1920,height=1080,framerate=10/1 ! h264parse ! avdec_h264 ! autovideoconvert ! gtksink sync=false
+
+gst-launch-1.0 -v -e v4l2src device=/dev/video1 ! queue ! video/x-h264,width=1280,height=720,framerate=5/1 ! h264parse ! avdec_h264 ! autovideoconvert ! gdkpixbufsink sync=false
+
+pipeline = "v4l2src device=/dev/video1 ! queue ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! avdec_h264 ! autovideoconvert ! videoscale ! capsfilter ! gtksink name=gtk sync=false"
+
+        
+gst-launch-1.0 -v -e v4l2src device=/dev/video1 ! queue ! video/x-h264,width=1280,height=720,framerate=5/1 ! h264parse ! avdec_h264 ! tee name=t ! queue ! autovideoconvert ! gtksink t. ! queue ! autovideoconvert ! gdkpixbufsink sync=false
+        
--- test.py
+++ test.py
@@ -9,23 +9,13 @@ from gi.repository import GdkPixbuf
 
 class GTK_Main(object):
     def __init__(self):
-        webcam = Gst.ElementFactory.make("v4l2src", "webcam")
-        
-        wrapper = Gst.ElementFactory.make("wrappercamerabinsrc", "wrapper")
-        wrapper.set_property('video-source', webcam)
-        camerabin = Gst.ElementFactory.make("camerabin", "camerabin")
-        camerabin.set_property('camera-source', wrapper)
-        self.player = Gst.Pipeline.new("player")
-        self.player.add(camerabin)
-        self.player.set_state(Gst.State.PLAYING)        
-        a = camerabin.get_property('image-capture-supported-caps').to_string().split(';')
-        print (a)
-        
         # Prepare the Gstreamer elements
-        pipeline = "uvch264src device=/dev/video1 mode=1 name=src src.vfsrc "
-        pipeline += "! video/x-raw,width=320,height=240,framerate=30/1 "
-        pipeline += "! gtksink sync=false name=gtk"
+        #pipeline = "uvch264src device=/dev/video1 name=src "
+        #pipeline += "src.imgsrc ! queue ! gdkpixbufsink sync=false "
+        #pipeline += "src.vfsrc ! queue ! video/x-raw,format=(string)BGRA,width=320,height=240,framerate=20/1 ! gtksink name=gtk sync=false "
+        pipeline = "v4l2src device=/dev/video1 ! queue ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! avdec_h264 ! tee name=t ! autovideoconvert ! queue ! gtksink name=gtk sync=false t. ! queue ! autovideoconvert ! videoscale ! capsfilter ! gdkpixbufsink name=buf sync=false"
         self.player = Gst.parse_launch(pipeline)
+        print (self.player)
         
         # Prepare the main window
         window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
@@ -48,52 +38,32 @@ class GTK_Main(object):
         bus.add_signal_watch()
         bus.enable_sync_message_emission()
         bus.connect("message", self.on_message)
-        bus.connect("sync-message::element", self.on_sync_message)
 
         self.player.set_state(Gst.State.PLAYING)        
 
-        camera = self.player.get_by_name('src')
-        m = camera.get_property('mode')
-        print (m)
-        modes=type(m)
-        still = modes(1)
-        print (still)
-        camera.set_property('mode', still)
-        
     def on_click(self, button):
         print ("Click !")
-        camera = self.player.get_by_name('src')
-        m = camera.get_property('mode')
-        modes=type(m)
-        still = modes(1)
-        camera.set_property('mode', still)
-        #camera.set_property('mode', GstCameraBin2Mode.Image)
-        print (camera.get_property('mode'))
-        print (camera.get_property('ready-for-capture'))
-        camera.emit('start-capture')
+        #camera = self.player.get_by_name('src')
+        #print (camera.get_property('mode'))
+        #print (camera.get_property('ready-for-capture'))
+        #camera.emit('start-capture')
+        #print (dir(camera))
+        #i = camera.get_static_pad('imgsrc')
+        #print (i)
+        #q = Gst.ElementFactory.make("queue")
+        #i.set_target(q)
+        p = self.player.get_by_name('buf')
+        buf = p.get_property('last-pixbuf')
+        buf.savev('test.png', 'png', ['compression', 'tEXt::Title'], ['3', 'test'])
         
     def on_message(self, bus, message):
         if message.type == Gst.MessageType.WARNING:
             err, debug = message.parse_warning()
-            print ("Error: %s" % err, debug)
-
-    def on_sync_message(self, bus, message):
-        struct = message.get_structure()
-        if not struct:
-            return
-        message_name = struct.get_name()
-        print (message_name)
-        print (message.src)
-        if message_name == "prepare-window-handle":
-            print (dir(message.src))
-            print (self.movie_window.window)
-            # Assign the viewport
-            imagesink = message.src
-            imagesink.set_property("force-aspect-ratio", True)
-            imagesink.set_xwindow_id(self.movie_window.window.xid)
+            print ("Warning: %s" % err, debug)
         
 
 GObject.threads_init()
-Gst.init(None)
+Gtk.init([])
+Gst.init([])
 GTK_Main()
 Gtk.main()
