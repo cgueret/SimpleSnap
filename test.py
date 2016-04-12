897690c5558ff7a65684b928feabfa9fb5dd097a
Christophe Gueret <christophe.gueret@gmail.com>
2016-04-12 21:06:14 +0100

    Added two working pipelines and removed the need for gtksink

--- test.py
+++ test.py
@@ -1,38 +1,62 @@
 #!/usr/bin/env python
+import os
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
 gi.require_version('GdkX11', '3.0')
 gi.require_version('GstVideo', '1.0')
-from gi.repository import Gst, GObject, Gtk, GdkX11, GstVideo
-from gi.repository import GdkPixbuf
+from gi.repository import GObject
+from gi.repository import Gst
+from gi.repository import Gtk
+from gi.repository import GdkX11    # for window.get_xid()
+from gi.repository import GstVideo  # for sink.set_window_handle()
+
+
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+def window_closed (widget, event, pipeline):
+    widget.hide()
+    pipeline.set_state(Gst.State.NULL)
+    Gtk.main_quit ()
 
 class GTK_Main(object):
     def __init__(self):
-        # Prepare the Gstreamer elements
-        #pipeline = "uvch264src device=/dev/video1 name=src "
-        #pipeline += "src.imgsrc ! queue ! gdkpixbufsink sync=false "
-        #pipeline += "src.vfsrc ! queue ! video/x-raw,format=(string)BGRA,width=320,height=240,framerate=20/1 ! gtksink name=gtk sync=false "
-        pipeline = "v4l2src device=/dev/video1 ! queue ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! avdec_h264 ! tee name=t ! autovideoconvert ! queue ! gtksink name=gtk sync=false t. ! queue ! autovideoconvert ! videoscale ! capsfilter ! gdkpixbufsink name=buf sync=false"
+        # Define the pipeline
+        pipeline_rpi = "v4l2src device=/dev/video0 ! queue ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! omxh264dec ! tee name=t ! queue ! autovideoconvert ! videoscale ! video/x-raw,width=320,height=240 ! ximagesink name=viewfinder sync=false t.  ! queue ! autovideoconvert ! gdkpixbufsink name=buf sync=false"
+        pipeline_laptop = "v4l2src device=/dev/video1 ! queue ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! avdec_h264 ! tee name=t ! queue ! autovideoconvert ! videoscale ! video/x-raw,width=768,height=576 ! glimagesink name=viewfinder sync=false t. ! queue ! autovideoconvert ! gdkpixbufsink name=buf sync=false"
+        pipeline = pipeline_laptop if os.path.exists('/dev/video1') else pipeline_rpi
         self.player = Gst.parse_launch(pipeline)
-        print (self.player)
         
         # Prepare the main window
         window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
         window.set_position(Gtk.WindowPosition.CENTER)
-        window.set_title("Video-Player")
+        window.set_title("SimpleSnap")
         window.set_default_size(500, 400)
-        window.connect("destroy", Gtk.main_quit, "WM destroy")
+        #window.connect("destroy", Gtk.main_quit, "WM destroy")
+        window.connect("delete-event", window_closed, self.player)
         vbox = Gtk.VBox()
         window.add(vbox)
+        drawing_area = Gtk.DrawingArea()
+        drawing_area.set_double_buffered (True)
+        vbox.pack_start(drawing_area, True, True, 0)
         hbox = Gtk.HBox()
-        vbox.add(self.player.get_by_name('gtk').get_property('widget'))
         vbox.pack_start(hbox, False, False, 0)
-        button = Gtk.Button("Click")
-        button.connect("clicked", self.on_click)
+        button = Gtk.Button("Take photo")
+        button.connect("clicked", self.on_take_photo)
+        hbox.pack_start(button, True, True, 0)
+        button = Gtk.Button("Quit")
+        button.connect("clicked", self.on_quit)
         hbox.pack_start(button, True, True, 0)
         window.show_all()
-                
+        window.fullscreen()
+        window.realize()
+        
+        # Bind the view finder sink to the drawing area
+        xid = drawing_area.get_window().get_xid()
+        view_finder = self.player.get_by_name('viewfinder')
+        view_finder.set_window_handle (xid)
+        
         # -> Listen to the bus
         bus = self.player.get_bus()
         bus.add_signal_watch()
@@ -41,20 +65,16 @@ class GTK_Main(object):
 
         self.player.set_state(Gst.State.PLAYING)        
 
-    def on_click(self, button):
+    def on_take_photo(self, button):
         print ("Click !")
-        #camera = self.player.get_by_name('src')
-        #print (camera.get_property('mode'))
-        #print (camera.get_property('ready-for-capture'))
-        #camera.emit('start-capture')
-        #print (dir(camera))
-        #i = camera.get_static_pad('imgsrc')
-        #print (i)
-        #q = Gst.ElementFactory.make("queue")
-        #i.set_target(q)
         p = self.player.get_by_name('buf')
         buf = p.get_property('last-pixbuf')
         buf.savev('test.png', 'png', ['compression', 'tEXt::Title'], ['3', 'test'])
+        
+    def on_quit(self, button):
+        print ("Bye !")
+        self.player.set_state(Gst.State.NULL)
+        Gtk.main_quit ()
         
     def on_message(self, bus, message):
         if message.type == Gst.MessageType.WARNING:
@@ -62,8 +82,9 @@ class GTK_Main(object):
             print ("Warning: %s" % err, debug)
         
 
-GObject.threads_init()
-Gtk.init([])
-Gst.init([])
-GTK_Main()
-Gtk.main()
+if __name__=="__main__":
+    GObject.threads_init()
+    Gtk.init([])
+    Gst.init([])
+    GTK_Main()
+    Gtk.main()
